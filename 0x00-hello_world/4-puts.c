8722557ce06013e3b478acf92463c7178829bcd5
gakii <gakii.kkirimi@gmail.com>
2023-03-16 14:41:10 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
 
-char str[] = "\"Programming is like building a multilingual puzzle";
-puts(str);
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
-return (0);
+	puts(str);
 
- }
+	return (0);
+
+}
