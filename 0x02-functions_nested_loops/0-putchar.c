2b28cc1dae3fcca9a4c3dc1b96d367e149d02bfc
gakiik <gakii.kkirimi@gmail.com>
2023-03-24 11:29:54 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+
+}
