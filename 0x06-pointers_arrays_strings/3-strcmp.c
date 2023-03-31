7f4bd4e954ca48cb43d88964f08e1a3cf220dae9
gakiik <gakii.kkirimi@gmail.com>
2023-03-31 05:15:34 -0700

    pointers,strings,arrays 2.0

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - compares string s1 and s2
+ *
+ * @s1: value from main
+ * @s2: value from main
+ *
+ * Return: value of difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+{
+return (0);
+}
+else
+{
+return (*s1 - *s2);
+}
+}
